f03077ff8e00864a258ea1e9fb64c67531caed54
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-27 10:43:16 -0300

    Falta Funcion

--- js/main.js
+++ js/main.js
@@ -3,21 +3,17 @@ let precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas: "));
 let cuotas = parseInt(prompt("ingrese la cantidad de cuotas a pagar: "));
 let inflacion = 6;
 let porcentaje = 0;
-    if((contado < precioCuotas) && (contado != 0) && (precioCuotas != 0) && (cuotas != 0)){
-        console.log("check 1");
+    while((contado < precioCuotas) && (contado != 0) && (precioCuotas != 0) && (cuotas != 0)){
         porcentaje=((((precioCuotas/cuotas) - (contado/cuotas)) / contado) * 100);
         console.log(porcentaje);
         if(porcentaje < inflacion){
             alert("Te conviene las cuotas mi rey");
-            console.log("check 2");
         }
         else{
             alert("No te conviene mi rey");
-            console.log("check 3");
         } 
+        break;
     }
-    else{
-        alert("No te conviene mi rey");
-    }
+
 
 
