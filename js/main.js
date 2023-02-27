3b8c3ab4c343dec79ef3768b90f01aa5b6f3c519
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-27 10:56:19 -0300

    Primer Pre-entrega

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ let precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas: "));
 let cuotas = parseInt(prompt("ingrese la cantidad de cuotas a pagar: "));
 let inflacion = 6;
 let porcentaje = 0;
-    while((contado < precioCuotas) && (contado != 0) && (precioCuotas != 0) && (cuotas != 0)){
-        porcentaje=((((precioCuotas/cuotas) - (contado/cuotas)) / contado) * 100);
+    function calcularPorcentaje(){
+        porcentaje = ((((precioCuotas/cuotas) - (contado/cuotas)) / contado) * 100);
         console.log(porcentaje);
+    }
+    while((contado < precioCuotas) && (contado != 0) && (precioCuotas != 0) && (cuotas != 0)){
+        calcularPorcentaje();
         if(porcentaje < inflacion){
             alert("Te conviene las cuotas mi rey");
         }
