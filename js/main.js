91b181170dffa8322a99e68b2203aa3577df36f7
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-27 10:36:10 -0300

    Falta Ciclo y Funcion

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@ let contado = parseFloat(prompt("Ingrese el precio de contado: "));
 let precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas: "));
 let cuotas = parseInt(prompt("ingrese la cantidad de cuotas a pagar: "));
 let inflacion = 6;
-let porcentaje;
+let porcentaje = 0;
     if((contado < precioCuotas) && (contado != 0) && (precioCuotas != 0) && (cuotas != 0)){
         console.log("check 1");
-        function recargo(){ 
-            ((precioCuotas - contado) / contado) * 100  == porcentaje;   
-        }
-        recargo();
+        porcentaje=((((precioCuotas/cuotas) - (contado/cuotas)) / contado) * 100);
         console.log(porcentaje);
         if(porcentaje < inflacion){
-            alert("Te conviene las cuotas mi rey")
+            alert("Te conviene las cuotas mi rey");
+            console.log("check 2");
+        }
+        else{
+            alert("No te conviene mi rey");
+            console.log("check 3");
         } 
     }
     else{
-        alert("no te conviene mi rey");
+        alert("No te conviene mi rey");
     }
 
 
